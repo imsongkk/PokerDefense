a7073e91f2f3bafe7e74ef14992bd94a123d1f0e
Iosif2510 <gpotatok@gmail.com>
2021-11-29 16:27:30 +0900

    PokerManager에 HandCalculate 함수 추가
    cardList: 플레이어가 최종 결정한 정렬되지 않은 카드 패 목록
    Hand: 타워의 종류를 결정할 수 있도록 패의 종류, 히든 여부, 하이 카드 값을 가지는 구조체
    HandCalculate: cardList를 Hand 구조체로 변환하는 함수

--- Assets/PokerDefense/Scripts/Managers/PokerManager.cs
+++ Assets/PokerDefense/Scripts/Managers/PokerManager.cs
@@ -2,6 +2,7 @@ using PokerDefense.Managers;
 using PokerDefense.Utils;
 using PokerDefense.UI.Scene;
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -23,27 +24,29 @@ public class PokerManager : MonoBehaviour
     public enum HandRank
     {
         HighCard,
-        Devil,
+        Beast,
         OnePair,
         TwoPair,
         Tripple,
         Straight,
         BackStraight,
         RoyalStraight,
-        Flush,
         FullHouse,
+        Flush,
         FourCards,
-        FiveCards,
         StraightFlush,
         BackStraightFlush,
         RoyalStraightFlush,
+        FiveCards,
         Jackpot
     }
 
-    public (CardShape, int)[] cardList = new (CardShape, int)[6];
+    //숫자: 1~8, Z는 8로 취급하여 저장
+    public List<(CardShape shape, int number)> cardList = new List<(CardShape shape, int number)>();
 
-    public HandRank[] hiddenRanks = {
-        HandRank.Devil,
+    public List<HandRank> hiddenRanks = new List<HandRank>
+    {
+        HandRank.Beast,
         HandRank.BackStraight,
         HandRank.RoyalStraight,
         HandRank.FiveCards,
@@ -56,10 +59,106 @@ public class PokerManager : MonoBehaviour
         HandRank handRank;  // 패의 종류
         bool isHidden;      // 히든 패인지 여부
         int topCard;        // 가장 높은 숫자
+
+        public Hand(HandRank handRank, bool isHidden, int topCard)
+        {
+            this.handRank = handRank;
+            this.isHidden = isHidden;
+            this.topCard = topCard;
+        }
     }
 
-    // public Hand HandCalaulate((CardShape, int)[] cardList)
-    // {
-    //     //TODO cardList의 카드를 읽어 어떤 패인지 계산하고 해당하는 Hand 구조체를 반환
-    // }
+    public Hand HandCalaulate(List<(CardShape shape, int number)> cardList)
+    {
+        cardList.Sort((x, y) => y.number.CompareTo(x.number));
+        int topCard = cardList[4].number;
+
+        List<int> sameNumbers = new List<int>();
+        int sameNumber = 1;
+        bool isFlush = true;
+        bool isStraight = true;
+        bool isBackStraight = false;
+
+        HandRank handRank;
+
+        for (int i = 1; i < 5; i++)
+        {
+            if (cardList[i].number == cardList[i - 1].number)
+            {
+                sameNumber++;
+                isStraight = false;
+            }
+            else
+            {
+                if (cardList[i].number != cardList[i - 1].number + 1)
+                {
+                    if ((i == 4) && isStraight && (cardList[i - 1].number == 4) && (cardList[i].number == 8))
+                    {
+                        isBackStraight = true;      // Back Straight
+                    }
+                    else isStraight = false;
+                }
+                sameNumbers.Add(sameNumber);
+                sameNumber = 1;
+            }
+            if ((cardList[i].shape != CardShape.Joker) && (cardList[i - 1].shape != CardShape.Joker) && (cardList[i].shape != cardList[i - 1].shape))
+            {
+                // Joker는 임의의 무늬로 취급
+                isFlush = false;
+            }
+        }
+
+        if (sameNumber == 5)
+        {
+            if (topCard == 7) handRank = HandRank.Jackpot;
+            else handRank = HandRank.FiveCards;
+        }
+        else if (sameNumbers.Contains(4))
+        {
+            handRank = HandRank.FourCards;
+        }
+        else if (sameNumbers.Contains(3))
+        {
+            if (sameNumbers.Contains(2)) handRank = HandRank.FullHouse;
+            else handRank = HandRank.Tripple;
+        }
+        else if (sameNumbers.Contains(2))
+        {
+            int pairs = 0;
+            foreach (int counts in sameNumbers)
+            {
+                if (counts == 2) pairs++;
+            }
+            if (pairs == 2) handRank = HandRank.TwoPair;
+            else handRank = HandRank.OnePair;
+        }
+        else
+        {
+            if (isStraight && isFlush)
+            {
+                if (isBackStraight) handRank = HandRank.BackStraightFlush;
+                else if (topCard == 8) handRank = HandRank.RoyalStraightFlush;
+                else handRank = HandRank.StraightFlush;
+            }
+            else if (isStraight)
+            {
+                if (isBackStraight) handRank = HandRank.BackStraight;
+                else if (topCard == 8) handRank = HandRank.RoyalStraight;
+                else handRank = HandRank.Straight;
+            }
+            else if (isFlush)
+            {
+                handRank = HandRank.Flush;
+            }
+            else
+            {
+                if (topCard == 6) handRank = HandRank.Beast;
+                else handRank = HandRank.HighCard;
+            }
+        }
+
+        Hand returnHand = new Hand(handRank, hiddenRanks.Contains(handRank), topCard);
+
+        return returnHand;
+    }
 }
